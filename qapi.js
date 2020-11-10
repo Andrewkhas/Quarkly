371e21ece24ecdee88a6f2220f277b8df378844f
Andrewkhas <ueferfrw@mail.ru>
2020-11-10 23:08:51 +0500

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5faacd5808b41a001febf7e9",
+				"5faacd5808b41a001febf7eb"
+			]
+		},
+		"5faacd5808b41a001febf7e9": {
+			"id": "5faacd5808b41a001febf7e9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5faacd5808b41a001febf7eb": {
+			"id": "5faacd5808b41a001febf7eb",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
